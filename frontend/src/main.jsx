8acbb1928dcf5e65ebc2331fda061d6190b71a1e
hassd973 <78494014+hassd973@users.noreply.github.com>
2025-04-03 20:10:56 +0100

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IceKingDashboard from './components/IceKingDashboard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IceKingDashboard />
+  </React.StrictMode>
+);
