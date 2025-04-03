9d23e104d077412e8c35702aba71552f700cb8b3
hassd973 <78494014+hassd973@users.noreply.github.com>
2025-04-03 20:47:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
