4d38b62a30615c6cf3ee30287b9114736cc49a1b
hassd973 <78494014+hassd973@users.noreply.github.com>
2025-04-05 01:46:01 +0100

    Update signer.ts
    Fix signer.ts for web3@1.7.4 compatibility

--- apex-sdk-node/src/pro/onboarding/eth-signing/signer.ts
+++ apex-sdk-node/src/pro/onboarding/eth-signing/signer.ts
@@ -1,7 +1,6 @@
 import { promisify } from 'es6-promisify';
 import { ethers } from 'ethers';
 import Web3 from 'web3';
-import { AbstractProvider } from 'web3-core';
 import { JsonRpcPayload, JsonRpcResponse } from 'web3-core-helpers';
 
 import { SignatureTypes, SigningMethod } from '../interface/main';
@@ -38,38 +37,42 @@ export abstract class Signer {
    */
   public abstract getDomainHash(): string;
 
-  protected async ethSignTypedDataInternal(signer: string, data: {}, signingMethod: SigningMethod): Promise<string> {
+  protected async ethSignTypedDataInternal(
+    signer: string,
+    data: {},
+    signingMethod: SigningMethod
+  ): Promise<string> {
     let rpcMethod: string;
     let rpcData: {};
 
-    let provider = this.web3.currentProvider as AbstractProvider & { request?: any };
-    if (provider === null) {
+    const provider = this.web3.currentProvider;
+    if (!provider) {
       throw new Error('Cannot sign since Web3 currentProvider is null');
     }
     if (typeof provider === 'string') {
       throw new Error('Cannot sign since Web3 currentProvider is a string');
     }
-    provider = provider as AbstractProvider;
+
     let sendAsync: (param: JsonRpcPayload) => Promise<JsonRpcResponse>;
 
     switch (signingMethod) {
       case SigningMethod.TypedData:
-        sendAsync = promisify(provider.send!).bind(provider);
+        sendAsync = promisify(provider.send!.bind(provider));
         rpcMethod = 'eth_signTypedData';
         rpcData = data;
         break;
       case SigningMethod.MetaMask:
-        sendAsync = promisify(provider.sendAsync).bind(provider);
+        sendAsync = promisify((provider as any).sendAsync.bind(provider));
         rpcMethod = 'eth_signTypedData_v3';
         rpcData = JSON.stringify(data);
         break;
       case SigningMethod.MetaMaskLatest:
-        sendAsync = promisify(provider?.sendAsync || provider?.send).bind(provider);
+        sendAsync = promisify(((provider as any).sendAsync || provider.send).bind(provider));
         rpcMethod = 'eth_signTypedData_v4';
         rpcData = JSON.stringify(data);
         break;
       case SigningMethod.CoinbaseWallet:
-        sendAsync = promisify(provider.sendAsync).bind(provider);
+        sendAsync = promisify((provider as any).sendAsync.bind(provider));
         rpcMethod = 'eth_signTypedData_v4';
         rpcData = data;
         break;
@@ -84,7 +87,7 @@ export abstract class Signer {
       id: Date.now(),
     });
 
-    const res = typeof response == 'string' ? { error: null, result: `${response}`.slice(2, 132) } : response;
+    const res = typeof response === 'string' ? { error: null, result: `${response}`.slice(2, 132) } : response;
     if (res.error) {
       throw new Error((res.error as unknown as { message: string }).message);
     }
@@ -99,18 +102,16 @@ export abstract class Signer {
     message: string,
     env?: ENV
   ): Promise<{ value: string; l2KeyHash: string }> {
-    let provider = this.web3.currentProvider as AbstractProvider & { request?: any };
-
-    if (provider === null) {
+    const provider = this.web3.currentProvider;
+    if (!provider) {
       throw new Error('Cannot sign since Web3 currentProvider is null');
     }
     if (typeof provider === 'string') {
       throw new Error('Cannot sign since Web3 currentProvider is a string');
     }
-    provider = provider as AbstractProvider;
 
     const sendAsync: (param: JsonRpcPayload) => Promise<JsonRpcResponse> = promisify(
-      provider.sendAsync || provider.send,
+      (provider as any).sendAsync || provider.send
     ).bind(provider);
 
     const rpcMethod = 'personal_sign';
@@ -118,10 +119,9 @@ export abstract class Signer {
 
     try {
       const msg = web3.utils.utf8ToHex(message);
-      // const r = await web3.eth.personal.sign(msg, web3.eth.accounts.wallet.[0].address, '')
-      if (web3.eth.accounts.wallet[0].privateKey) {
+      if (web3.eth.accounts.wallet[0]?.privateKey) {
         const tempRes = await web3.eth.accounts.sign(msg, web3.eth.accounts.wallet[0].privateKey);
-        response = tempRes.signature
+        response = tempRes.signature;
       } else {
         response = await sendAsync({
           method: rpcMethod,
@@ -138,13 +138,11 @@ export abstract class Signer {
     const verifiedAddress = ethers.utils.verifyMessage(message, signedMsg);
 
     const ifValid = verifiedAddress.toLowerCase() === signer.toLowerCase();
-
     if (!ifValid) {
       throw new Error('Invalid signature');
     }
 
-    const res = typeof response == 'string' ? { error: null, result: `${response}`.slice(2, 132) } : response;
-
+    const res = typeof response === 'string' ? { error: null, result: `${response}`.slice(2, 132) } : response;
     if (res.error) {
       throw new Error((res.error as unknown as { message: string }).message);
     }
@@ -152,17 +150,18 @@ export abstract class Signer {
     const kL2KeyHashProd = '0x3978602b67f89ae820dcc57869dfab215c0a48f7510d95baef4cef262ad38350';
     const kL2KeyHashTestnet = '0x0be1ca974483d76bfb1b0b934b192f880e1e64c4872bfe471402337a70736366';
 
-    const kL2KeyHash = env.isProd ? kL2KeyHashProd : kL2KeyHashTestnet;
+    const kL2KeyHash = env && env.isProd ? kL2KeyHashProd : kL2KeyHashTestnet;
 
     const bytes = ethers.utils.toUtf8Bytes(message);
     const personalSignMessageHash = ethers.utils.sha256(bytes);
 
-    if (
-      !ethers.BigNumber.from(personalSignMessageHash).eq(kL2KeyHash)
-    ) {
+    if (!ethers.BigNumber.from(personalSignMessageHash).eq(kL2KeyHash)) {
       throw new Error('personal_sign content hash mismatch');
     }
-    // Note: Using createTypedSignature() fixes the signature `v` value.
-    return { value: createTypedSignature(res.result, SignatureTypes.PERSONAL), l2KeyHash: personalSignMessageHash };
+
+    return {
+      value: createTypedSignature(res.result, SignatureTypes.PERSONAL),
+      l2KeyHash: personalSignMessageHash,
+    };
   }
 }
