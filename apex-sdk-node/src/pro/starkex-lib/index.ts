ba84472ab42cc6c455ca8d23f4d612e4e3d6b5e7
hassd973 <78494014+hassd973@users.noreply.github.com>
2025-04-12 20:59:46 +0100

    Update index.ts
    Fix ESM imports in index.ts to use helpers.ts

--- apex-sdk-node/src/pro/starkex-lib/index.ts
+++ apex-sdk-node/src/pro/starkex-lib/index.ts
@@ -1,21 +1,20 @@
 // apex-sdk-node/src/pro/starkex-lib/index.ts
+export * from './constants';
+export * from './helpers';
+export * from './keys';
+export * from './lib';
+export * from './signable';
+export * from './types';
+export * from './main';
 
-export * from './constants.js';
-export * from './helpers.js';
-export * from './keys.js';
-export * from './lib.js';
-export * from './signable.js';
-export * from './types.js';
-export * from './main.js';
-
-// Explicit named exports (useful for IDE autocomplete and tree-shaking)
+// Explicit named exports
 export { 
   addOrderExpirationBufferHours, 
   isoTimestampToEpochHours,
   stripHexPrefix,
   asEcKeyPair,
   asSimpleKeyPair
-} from './helpers.js';
+} from './helpers';
 
 export {
   setConfig,
@@ -27,8 +26,7 @@ export {
   setSymbolsV2,
   genSimplifyOnBoardingSignature,
   keyPairFromData,
-  KeyPair // export named if needed alongside *
-} from './lib.js';
+  KeyPair
+} from './lib';
 
-export { SignableOrder } from './signable.js';
-
+export { SignableOrder } from './signable';
