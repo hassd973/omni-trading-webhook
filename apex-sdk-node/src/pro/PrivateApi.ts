d16d19fe639cb6fd3ca7b0f4c277d1c5b7f38501
hassd973 <78494014+hassd973@users.noreply.github.com>
2025-04-13 18:19:03 +0100

    Update PrivateApi.ts
    Fix PrivateApi.ts clientId, side, and expiration errors

--- apex-sdk-node/src/pro/PrivateApi.ts
+++ apex-sdk-node/src/pro/PrivateApi.ts
@@ -11,7 +11,6 @@ import {
   ISO8601,
   Market,
   OrderObject,
-  OrderResponseObject,
   OrderSide,
   OrderWithClientId,
   Trace,
@@ -94,27 +93,17 @@ export class PrivateApi {
     return await signatureFunc();
   }
 
-  /**
-   * GET Retrieve User Data
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-retrieve-user-data
-   */
   async user(): Promise<UserObject> {
     return this.request('/api/v1/user', 'get');
   }
 
-  /**
-   * GET Retrieve User Account Data
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-retrieve-user-data
-   * @param id  accountId
-   * @param ethereumAddress ethereumAddress
-   * @returns promise
-   */
   async getAccount(id: string, ethereumAddress: string): Promise<AccountObject> {
     return this.request('/api/v1/account', 'get', {
       id,
       ethereumAddress,
     });
   }
+
   async getAccountV2(id: string, ethereumAddress: string): Promise<AccountObject> {
     return this.request('/api/v2/account', 'get', {
       id,
@@ -122,18 +111,6 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET Trade History
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-trade-history
-   * @param symbol
-   * @param status
-   * @param side 'BUY' | 'SELL'
-   * @param limit default at 100
-   * @param beginTimeInclusive Start time
-   * @param endTimeExclusive End time
-   * @param page Page numbers start from 0
-   * @param orderType "ACTIVE","CONDITION","HISTORY"
-   */
   async tradeHistory(
     symbol?: string,
     status?: 'PENDING' | 'OPEN' | 'FILLED' | 'CANCELED' | 'EXPIRED' | 'UNTRIGGERED',
@@ -180,13 +157,6 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET Worst Price
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-worst-price
-   * @param symbol Symbol
-   * @param size Order open size
-   * @param side BUY or SELL order
-   */
   async getWorstPrice(symbol: string, size: string, side: 'BUY' | 'SELL'): Promise<WorstPriceObject> {
     return this.request('/api/v1/get-worst-price', 'get', {
       symbol,
@@ -203,24 +173,8 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * POST Creating Orders
-   * @see https://api-docs.pro.apex.exchange/#privateapi-post-creating-orders
-   * @param clientOrderId Randomized client id
-   * @param positionId
-   * @param symbol Symbol
-   * @param side BUY or SELL
-   * @param type  "LIMIT", "MARKET","STOP_LIMIT", "STOP_MARKET", "TAKE_PROFIT_LIMIT", "TAKE_PROFIT_MARKET"
-   * @param size Size
-   * @param price Price
-   * @param limitFee  limitFee = price * size * takerFeeRate  ( from GET /v1/account)
-   * @param timeInForce "GOOD_TIL_CANCEL", "FILL_OR_KILL", "IMMEDIATE_OR_CANCEL", "POST_ONLY"
-   * @param triggerPrice Trigger price
-   * @param trailingPercent Conditional order trailing-stop
-   * @param reduceOnly Reduce-only
-   */
   async createOrder(
-    clientOrderId: string,
+    clientId: string,
     positionId: string,
     symbol: string,
     side: 'BUY' | 'SELL',
@@ -234,27 +188,26 @@ export class PrivateApi {
     reduceOnly?: boolean,
     brokerId?: string
   ): Promise<OrderObject> {
-    clientOrderId = clientOrderId || generateRandomClientId();
-    const expirationIsoTimestamp = (Date.now() + 30 * 24 * 60 * 60 * 1000) as any;
-    const signature: string = await this.getSignature('', () => {
+    clientId = clientId || generateRandomClientId();
+    const expirationIsoTimestamp = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString();
+    const signature: string = await this.getSignature('', async () => {
       const orderToSign: OrderWithClientId = {
-  clientOrderId,
-  humanSize: `${Number(size)}`,
-  humanPrice: price,
-  limitFee,
-  symbol,
-  side: side === 'BUY' ? OrderSide.BUY : OrderSide.SELL,
-  expirationIsoTimestamp,
-  clientId: clientOrderId,
-  positionId,
-};
+        positionId,
+        humanSize: size,
+        limitFee,
+        symbol,
+        side,
+        expirationIsoTimestamp,
+        clientId,
+        humanPrice: price,
+      };
 
       const starkOrder = SignableOrder.fromOrder(orderToSign, this.clientConfig.networkId);
       return starkOrder.sign(this.clientConfig.starkKeyPair);
     });
     const order = {
-      clientId: clientOrderId,
-      expiration: addOrderExpirationBufferHours(isoTimestampToEpochHours(expirationIsoTimestamp)) * 60 * 60 * 1000,
+      clientId,
+      expiration: new Date(addOrderExpirationBufferHours(expirationIsoTimestamp)).getTime(),
       limitFee,
       price,
       reduceOnly,
@@ -262,7 +215,6 @@ export class PrivateApi {
       signature,
       size,
       symbol,
-      clientOrderId,
       timeInForce,
       triggerPrice,
       trailingPercent,
@@ -273,7 +225,7 @@ export class PrivateApi {
   }
 
   async createOrderV2(
-    clientOrderId: string,
+    clientId: string,
     positionId: string,
     symbol: string,
     side: 'BUY' | 'SELL',
@@ -287,27 +239,26 @@ export class PrivateApi {
     reduceOnly?: boolean,
     brokerId?: string
   ): Promise<OrderObject> {
-    clientOrderId = clientOrderId || generateRandomClientId();
-    const expirationIsoTimestamp = (Date.now() + 30 * 24 * 60 * 60 * 1000) as any;
-    const signature: string = await this.getSignature('', () => {
+    clientId = clientId || generateRandomClientId();
+    const expirationIsoTimestamp = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString();
+    const signature: string = await this.getSignature('', async () => {
       const orderToSign: OrderWithClientId = {
-  clientOrderId,
-  humanSize: `${Number(size)}`,
-  humanPrice: price,
-  limitFee,
-  symbol,
-  side: side === 'BUY' ? OrderSide.BUY : OrderSide.SELL,
-  expirationIsoTimestamp,
-  clientId: clientOrderId,
-  positionId,
-};
+        positionId,
+        humanSize: size,
+        limitFee,
+        symbol,
+        side,
+        expirationIsoTimestamp,
+        clientId,
+        humanPrice: price,
+      };
 
       const starkOrder = SignableOrder.fromOrder(orderToSign, this.clientConfig.networkId);
       return starkOrder.sign(this.clientConfig.starkKeyPair);
     });
     const order = {
-      clientId: clientOrderId,
-      expiration: addOrderExpirationBufferHours(isoTimestampToEpochHours(expirationIsoTimestamp)) * 60 * 60 * 1000,
+      clientId,
+      expiration: new Date(addOrderExpirationBufferHours(expirationIsoTimestamp)).getTime(),
       limitFee,
       price,
       reduceOnly,
@@ -315,7 +266,6 @@ export class PrivateApi {
       signature,
       size,
       symbol,
-      clientOrderId,
       timeInForce,
       triggerPrice,
       trailingPercent,
@@ -325,11 +275,6 @@ export class PrivateApi {
     return this.request('/api/v2/create-order', 'post', order);
   }
 
-  /**
-   * POST Cancel Order
-   * @see https://api-docs.pro.apex.exchange/#privateapi-post-cancel-order
-   * @param id of the order being canceled
-   */
   async cancelOrder(id: string): Promise<number> {
     return this.request('/api/v1/delete-order', 'post', {
       id,
@@ -342,11 +287,6 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * Cancel Order By ClientOrderId
-   * @see https://api-docs.pro.apex.exchange/#privateapi-post-cancel-order-by-clientorderid
-   * @param id of the order being canceled
-   */
   async cancelOrderByClientOrderId(id: string): Promise<number> {
     return this.request('/api/v1/delete-client-order-id', 'post', {
       id,
@@ -359,16 +299,12 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * POST Cancel all Open Orders
-   * @see https://api-docs.pro.apex.exchange/#privateapi-post-cancel-all-open-orders
-   * @param symbol "BTC-USDC,ETH-USDC", Cancel all orders if none
-   */
   async cancelAllOrder(symbol?: string): Promise<void> {
     return this.request('/api/v1/delete-open-orders', 'post', {
       symbol,
     });
   }
+
   async cancelAllOrderV2(token: 'USDC' | 'USDT', symbol?: string): Promise<void> {
     return this.request('/api/v2/delete-open-orders', 'post', {
       token,
@@ -376,50 +312,26 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET Open Orders
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-open-orders
-   */
   async openOrders(): Promise<{ orders: OrderObject[] }> {
-    return this.request('/api/v1/open-orders', 'get', { });
+    return this.request('/api/v1/open-orders', 'get', {});
   }
+
   async openOrdersV2(token: 'USDC' | 'USDT'): Promise<{ orders: OrderObject[] }> {
     return this.request('/api/v1/open-orders', 'get', { token });
   }
 
-  /**
-   * GET Order ID
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-order-id
-   * @param id
-   */
   async getOrder(id: string): Promise<OrderObject> {
     return this.request('/api/v1/get-order', 'get', { id });
   }
+
   async getOrderV2(id: string): Promise<OrderObject> {
     return this.request('/api/v2/get-order', 'get', { id });
   }
 
-  /**
-   * GET Order by clientOrderId
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-order-by-clientorderid
-   * @param id
-   */
   async getOrderByClientOrderId(id: string): Promise<OrderObject> {
     return this.request('/api/v1/order-by-client-order-id', 'get', { id });
   }
 
-  /**
-   * GET All Order History
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-all-order-history
-   * @param symbol
-   * @param status
-   * @param side 'BUY' | 'SELL'
-   * @param limit default at 100
-   * @param beginTimeInclusive Start time
-   * @param endTimeExclusive End time
-   * @param page Page numbers start from 0
-   * @param orderType "ACTIVE","CONDITION","HISTORY"
-   */
   async historyOrders(
     symbol?: string,
     status?: 'PENDING' | 'OPEN' | 'FILLED' | 'CANCELED' | 'EXPIRED' | 'UNTRIGGERED',
@@ -466,17 +378,6 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET Funding Rate
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-funding-rate
-   * @param symbol
-   * @param limit default at 100
-   * @param page Page numbers start from 0
-   * @param beginTimeInclusive Start time
-   * @param endTimeExclusive End time
-   * @param side 'BUY' | 'SELL'
-   * @param status
-   */
   async fundingRate(
     symbol?: string,
     limit?: number,
@@ -496,6 +397,7 @@ export class PrivateApi {
       status,
     });
   }
+
   async fundingRateV2(
     token: 'USDC' | 'USDT',
     symbol?: string,
@@ -518,16 +420,6 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET User Historial Profit and Loss
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-user-historial-profit-and-loss
-   * @param beginTimeInclusive Start time
-   * @param endTimeExclusive End time
-   * @param type Position type
-   * @param symbol Symbol
-   * @param page Page numbers start from 0
-   * @param limit Default at 100
-   */
   async historicalPNL(
     beginTimeInclusive?: number,
     endTimeExclusive?: number,
@@ -545,6 +437,7 @@ export class PrivateApi {
       limit,
     });
   }
+
   async historicalPNLV2(
     token: 'USDC' | 'USDT',
     beginTimeInclusive?: number,
@@ -565,29 +458,21 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET Yesterday's Profit & Loss
-   * @see https://api-docs.pro.apex.exchange/#privateapi-get-yesterday-39-s-profit-amp-loss
-   */
   async yesterdayPNL(): Promise<string> {
-    return this.request('/api/v1/yesterday-pnl', 'get', { });
+    return this.request('/api/v1/yesterday-pnl', 'get', {});
   }
+
   async yesterdayPNLV2(token: 'USDC' | 'USDT'): Promise<string> {
     return this.request('/api/v2/yesterday-pnl', 'get', { token });
   }
 
-  /**
-   * POST Sets the initial margin rate of a contract
-   * @see https://api-docs.pro.apex.exchange/#privateapi-post-sets-the-initial-margin-rate-of-a-contract
-   * @param symbol
-   * @param initialMarginRate
-   */
   async setInitialMarginRate(symbol: string, initialMarginRate: string): Promise<void> {
     return this.request('/api/v1/set-initial-margin-rate', 'post', {
       symbol,
       initialMarginRate,
     });
   }
+
   async setInitialMarginRateV2(symbol: string, initialMarginRate: string): Promise<void> {
     return this.request('/api/v2/set-initial-margin-rate', 'post', {
       symbol,
@@ -595,12 +480,10 @@ export class PrivateApi {
     });
   }
 
-  /**
-   * GET Account Balance
-   */
   async accountBalance(): Promise<AccountBalanceObject> {
     return this.request('/api/v1/account-balance', 'get', {});
   }
+
   async accountBalanceV2(): Promise<AccountBalanceObject> {
     return this.request('/api/v2/account-balance', 'get', {});
   }
